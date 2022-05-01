fb60589f98aa60a89d213af325c420d2db79f0ae
Yash <yash@gmail.com>
2022-05-01 15:07:39 +0530

    Piped Output and Input Stream

---
+++ PipedStream.java
@@ -0,0 +1,80 @@
+package Java_IO_Stream;
+import java.io.*;
+
+
+
+//creating first thread for producer
+
+class Producer extends Thread{
+
+    OutputStream os;
+
+    public Producer(OutputStream o){
+        os=o;
+    }
+
+    public void run(){
+        int count=1;
+        while(true){
+            try{
+                os.write(count);
+                os.flush();
+                System.out.println("Producer "+count);
+                System.out.flush();
+                Thread.sleep(10);
+                count++;
+            }
+            catch(Exception e){
+
+            }
+            
+            
+        }
+    }
+}
+
+//Creating second thread for consumer 
+
+class Consumer extends Thread{
+
+    InputStream is;
+
+    public Consumer(InputStream s){
+        is=s;
+    }
+
+    public void run(){
+        int x;
+        while(true){
+            try{
+                x=is.read();
+                System.out.println("Consumer "+x);
+                System.out.flush();
+                Thread.sleep(10);
+            }
+            catch(Exception e){
+
+            }
+            
+            
+        }
+    }
+}
+
+public class PipedStream {
+    
+    public static void main(String[] args) throws Exception{
+        PipedInputStream pis = new PipedInputStream();
+        PipedOutputStream pos=new PipedOutputStream();
+
+        pos.connect(pis);  // We can connect them in any way means pis.connect(pos) is also correct
+        
+        Producer p=new Producer(pos);
+        Consumer c=new Consumer(pis);
+
+        p.start();
+        c.start();
+
+
+    }
+}
