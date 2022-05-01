8e0b55727737fdbc354a76b0aea0807cfe89214d
Yash <yash@gmail.com>
2022-05-01 15:39:49 +0530

    Random access file class

--- Randomaccess.java
+++ Randomaccess.java
@@ -18,5 +18,6 @@ public class Randomaccess {
         System.out.println(rf.getFilePointer());
         rf.seek(rf.getFilePointer()+2);
         System.out.println((char)rf.read());
+        rf.close();
     }
 }
